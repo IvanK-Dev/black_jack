5418e0d7ba7082f1bcd4c63aa443e29f54ac199a
IvanK-Dev <zak98k@ukr.net>
2023-11-08 11:45:52 +0200

    add JsDoc

--- src/js/helpers/showWinnersElement.js
+++ src/js/helpers/showWinnersElement.js
@@ -1,19 +1,33 @@
 import Dealer from '../Dealer.js';
 
+/**
+ * Функция для создания элемента отображения победителей игры.
+ * @param {object} game - Объект игры Blackjack.
+ * @returns {HTMLDivElement} - HTML-элемент, содержащий информацию о победителях.
+ */
 export const showWinnersElement = (game) => {
+  // Фильтрация игроков с количеством очков <= 21
   let players = game.players.filter((player) => player.score <= 21);
+
+  // Поиск максимального количества очков среди игроков
   const winScore = Math.max(...players.map(({ score }) => score));
+
+  // Фильтрация игроков с количеством очков, равным максимальному
   players = players.filter((player) => player.score === winScore);
-  const winnersHTMLStr = players.map((player) => {
-    if (player instanceof Dealer) {
-      return `<p>Диллер выиграл. Очки: ${player.score} </p>`;
-    }
-    else {
-      return `<p>Игрок ${player.id} выиграл. Очки: ${player.score} </p>`;
-    }
-  }).join('');
-  const winnersElement=document.createElement('div')
-  winnersElement.className='winners'
-  winnersElement.innerHTML=winnersHTMLStr
-return winnersElement
+
+  // Создание HTML строки с информацией о победителях
+  const winnersHTMLStr = players
+    .map((player) => {
+      if (player instanceof Dealer) {
+        return `<p>Диллер выиграл. Очки: ${player.score} </p>`;
+      } else {
+        return `<p>Игрок ${player.id} выиграл. Очки: ${player.score} </p>`;
+      }
+    })
+    .join('');
+  // Создание HTML-элемента div для отображения информации о победителях
+  const winnersElement = document.createElement('div');
+  winnersElement.className = 'winners';
+  winnersElement.innerHTML = winnersHTMLStr;
+  return winnersElement;
 };
--- src/js/helpers/waitForEndGame.js
+++ src/js/helpers/waitForEndGame.js
@@ -1,15 +1,25 @@
-export function waitForEndGame(game){
+/**
+ * Асинхронная функция для ожидания завершения игры.
+ * @param {object} game - Объект игры.
+ * @returns {Promise<boolean>} - Промис, который разрешается значением true при завершении игры.
+ */
+export function waitForEndGame(game) {
   return new Promise((resolve) => {
+    /**
+     * Функция проверки окончания игры.
+     */
     const checkStopped = () => {
-        console.log('waitForEndGame start game',game)
+      console.log('waitForEndGame начало игры', game);
 
       if (game && game.endGame) {
-        console.log('waitForEndGame')
+        console.log('waitForEndGame');
         resolve(true);
       } else {
-        setTimeout(checkStopped, 1000); // Проверка каждые 100 миллисекунд
+        setTimeout(checkStopped, 1000); // Проверка каждую секунду (1000 миллисекунд)
       }
     };
+
+    // Вызов для проверки завершения игры
     checkStopped();
   });
-};
+}
